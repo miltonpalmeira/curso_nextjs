03803ea9948440f450413022e112fb1a530529dd
miltonpalmeira​ <lpsantana_@hotmail.com>
2023-11-17 12:00:39 -0300

    add(sobre) - adicionada página sobre e config typescript

--- primeiro_exemplo/src/app/page.tsx
+++ primeiro_exemplo/src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-
 export default function Home() {
   const myAge = 25;
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <div>
       <h1>Hello World!</h1>
       <p>Welcome to NextJS.</p>
       <p>Your age is ${myAge}</p>
-    </main>
+    </div>
   )
 }
